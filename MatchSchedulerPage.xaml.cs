6997dcd4b216b82059cc2bf4c9d827404f882196
aljohnston112 <aljohnston112@gmail.com>
2024-10-14 14:52:35 -0500

    Added a Match class to store matches between two players and the estimated time of the Match.
    Added the match scheduler page.

---
+++ MatchSchedulerPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CS341Project.Models;
+
+namespace CS341Project;
+
+/// <summary>
+/// A page that displays a potential match schedule
+/// for all matches in a given tournament.
+/// </summary>
+public partial class MatchSchedulerPage : ContentPage
+{
+
+    public ObservableCollection<Match> Matches { get; } = [];
+
+    public MatchSchedulerPage()
+    {
+        InitializeComponent();
+        MatchList.BindingContext = this;
+        for (var i = 0; i < 10; i++)
+        {
+            Matches.Add(new Match("A", "E", 100));
+            Matches.Add(new Match("B", "F", 101));
+            Matches.Add(new Match("C", "G", 102));
+            Matches.Add(new Match("D", "H", 103));
+        }
+
+    }
+}
---
+++ Models/Match.cs
@@ -0,0 +1,35 @@
+﻿namespace CS341Project.Models;
+
+/// <summary>
+/// Represents a match between two players with a time estimate
+/// on how long the match could take.
+/// </summary>
+/// <param name="player1">The first player.</param>
+/// <param name="player2">The second player.</param>
+/// <param name="estimatedSeconds">
+/// The estimated time, in seconds,
+/// of the match between the two given players.
+/// </param>
+public class Match(string player1, string player2, long estimatedSeconds)
+{
+    public Player Player1 { get; }
+
+    public Player Player2 { get; }
+
+    /// <summary>
+    /// Provides the estimated time in mm:ss format
+    /// where mm are the minutes and ss are the seconds.
+    /// mm may be more than two characters
+    /// </summary>
+    public string EstimatedTime
+    {
+        get
+        {
+            var minutes = estimatedSeconds / 60;
+            var seconds = estimatedSeconds % 60;
+            return $"{minutes}:{seconds:00}";
+        }
+    }
+
+    public string FormattedPlayers => $"{player1} vs. {player2}";
+}
