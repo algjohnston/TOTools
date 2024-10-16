b0cc0ac7ef9f66358a5d15a96f95a9d957d02516
whodisgui <elliottdolezal@gmail.com>
2024-10-16 17:04:16 -0500

    TitlePage Implement Phase 1

--- HomePage.xaml.cs
+++ HomePage.xaml.cs
@@ -11,8 +11,7 @@ public partial class HomePage : ContentPage {
 
     private void OnLogOutButtonClicked(object? sender, EventArgs e)
     {
-        // throw new NotImplementedException();
-        Navigation.PushAsync(new SignUpPage()); //goes to sign up page for now
+		Navigation.PushAsync(new TitleLoginSignup.TitlePage());
     }
 
     private void OnSeedingButtonClicked(object? sender, EventArgs e)
