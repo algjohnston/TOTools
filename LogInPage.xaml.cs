2921cce7a9cd0f471e8f556f5bc266dc554b3843
Elliott Dolezal <elliottdolezal@gmail.com>
2024-10-16 17:06:29 -0500

    TitlePage Implement Phase 2
    The XAML and XAML.CS files for each Title, SignUp, and LogIn

---
+++ LogInPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace CS341Project.TitleLoginSignup;
+
+public partial class LogInPage : ContentPage
+{
+	public LogInPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnLogInButtonClicked(object? sender, EventArgs e)
+	{
+		Navigation.PushAsync(new HomePage());
+	}
+
+	private void OnCancelButtonClicked(object? sender, EventArgs e)
+	{
+		Navigation.PushAsync(new TitleLoginSignup.TitlePage());
+	}
+
+}
---
+++ TitlePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace CS341Project.TitleLoginSignup;
+
+public partial class TitlePage : ContentPage
+{
+	public TitlePage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnLogInButtonClicked(object? sender, EventArgs e)
+	{
+		Navigation.PushAsync(new TitleLoginSignup.LogInPage());
+	}
+
+	private void OnSignUpButtonClicked(object? sender, EventArgs e)
+	{
+		Navigation.PushAsync(new TitleLoginSignup.SignUpPage());
+	}
+
+	private void OnSkipButtonClicked(object? sender, EventArgs e)
+	{
+		Navigation.PushAsync(new EventMap.EventMapPage());
+	}
+
+}
