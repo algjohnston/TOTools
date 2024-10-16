5c179f53522e5436bcce8fee1fa43278a2778680
DeductedPluto31 <sabelm85@uwosh.edu>
2024-10-16 13:18:24 -0500

    Added SignUpPage Folder and started working on it's internal xaml and xaml.cs files

---
+++ SignUp Page/SignUpPage.xaml.cs
@@ -0,0 +1,18 @@
+
+
+namespace CS341Project;
+
+public partial class SignUpPage : ContentPage {
+
+    public SignUpPage () {
+        InitializeComponent();
+    }
+
+
+    private void OnOkButtonClicked(object? sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    
+}
