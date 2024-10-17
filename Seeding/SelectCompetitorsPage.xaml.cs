eb6aa14a7f0f31f26c974b0b22827bf1b1ea72a6
aljohnston112 <aljohnston112@gmail.com>
2024-10-16 22:17:25 -0500

    Added the list functionality to SelectCompetitorsPage.

--- Seeding/SelectCompetitorsPage.xaml.cs
+++ Seeding/SelectCompetitorsPage.xaml.cs
@@ -1,10 +1,20 @@
-﻿namespace CS341Project.Seeding;
+﻿using System.Collections.ObjectModel;
+
+namespace CS341Project.Seeding;
 
 public partial class SelectCompetitorsPage : ContentPage
 {
+
+	public ObservableCollection<string> Competitors { get; } = [];
 	public SelectCompetitorsPage()
 	{
 		InitializeComponent();
+		BindingContext = this;
+		Competitors.Add("Player 1");
+		Competitors.Add("Player 2");
+		Competitors.Add("Player 3");
+		Competitors.Add("Player 4");
+		Competitors.Add("Player 5");
 	}
 
 	private void OnSubmitClicked(object sender, EventArgs e)
