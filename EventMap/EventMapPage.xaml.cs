ce4e6a76b612d2f5a82bdf89c2d29f8b8b567efa
aljohnston112 <aljohnston112@gmail.com>
2024-10-28 15:15:43 -0500

    Added location permissions. Got the map to display once, but it does not work.

--- EventMap/EventMapPage.xaml.cs
+++ EventMap/EventMapPage.xaml.cs
@@ -1,18 +1,67 @@
+using BruTile.Wms;
+
 namespace CS341Project.EventMap;
 
 public partial class EventMapPage : ContentPage
 {
-	
-	public EventMapPage()
-	{
-		// Initializes and displays map
-		InitializeComponent();
-	}
+    public EventMapPage()
+    {
+        InitializeComponent();
+        
+        // Works on Windows 10, but not Android 14
+        var mapControl = new Mapsui.UI.Maui.MapControl();
+        mapControl.Map.Layers.Add(
+            Mapsui.Tiling.OpenStreetMap.CreateTileLayer()
+            );
+        EventMapGrid.Add(mapControl, row:0);
+    }
+    
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await CheckAndRequestLocationPermission();
+    }
 
-	private void OnViewAllEventsButtonClicked(object? sender, EventArgs e)
-	{
-		
-			Navigation.PushAsync(new EventListPage());
 
-	}
+    public async Task<PermissionStatus> CheckAndRequestLocationPermission()
+    {
+        PermissionStatus status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+
+        if (status == PermissionStatus.Granted)
+        {
+            return status;
+        }
+
+        if (status == PermissionStatus.Denied)
+        {
+            await DisplayAlert(
+                "Permission Needed",
+                "You need to enable location to see nearby events." +
+                "Your location data will not be stored or transmitted in any way.",
+                "OK"
+            );
+            status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+
+            return status;
+        }
+
+        if (Permissions.ShouldShowRationale<Permissions.LocationWhenInUse>())
+        {
+            await DisplayAlert(
+                "Permission Rationale",
+                "Your location is used to determine which events are nearby. " +
+                "Your location data will not be stored or transmitted in any way.",
+                "OK"
+            );
+        }
+
+        status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+
+        return status;
+    }
+
+    private void OnViewAllEventsButtonClicked(object? sender, EventArgs e)
+    {
+        Navigation.PushAsync(new EventListPage());
+    }
 }
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using SkiaSharp.Views.Maui.Controls.Hosting;
 using StrawberryShake;
 
 namespace CS341Project;
@@ -10,6 +11,7 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .UseSkiaSharp()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
