314926e59590e2a24e8a5033eeab9e8ecfa99b3a
DeductedPluto31 <sabelm85@uwosh.edu>
2024-11-16 22:52:38 -0600

    Changed LoginPage, TitlePage, and HomePage colors and background. Also edited Colors.xaml.

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
 
 namespace TOTools;
 
@@ -9,4 +10,16 @@ namespace TOTools;
                            ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        // Hide the status bar to achieve fullscreen
+        Window.DecorView.SystemUiVisibility = (StatusBarVisibility)
+            (SystemUiFlags.LayoutStable | SystemUiFlags.LayoutFullscreen | SystemUiFlags.HideNavigation);
+
+        // Ensure fullscreen mode
+        Window.AddFlags(WindowManagerFlags.Fullscreen);
+        Window.ClearFlags(WindowManagerFlags.ForceNotFullscreen);
+    }
 }
