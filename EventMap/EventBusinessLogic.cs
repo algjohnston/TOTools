a5755143ff624ca75eb30dd60349ec6d9864c2b8
caden <rohanc94@uwosh.edu>
2024-12-12 15:26:29 -0600

    missed some stuff

--- EventMap/EventBusinessLogic.cs
+++ EventMap/EventBusinessLogic.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.ObjectModel;
+using Mapsui.Tiling;
 using TOTools.Database;
 using TOTools.Models;
-
 namespace TOTools.EventMap;
 
 /// <summary>
@@ -11,7 +11,6 @@ namespace TOTools.EventMap;
 public class EventBusinessLogic(EventTable eventTable)
 {
     public ObservableCollection<Event> Events { get; } = [];
-
     private readonly TaskCompletionSource<bool> _loadCompletionSource = new();
     public Task LoadTask => _loadCompletionSource.Task;
 
@@ -31,4 +30,41 @@ public class EventBusinessLogic(EventTable eventTable)
 
         _loadCompletionSource.TrySetResult(true);
     }
+
+    public async void AddEvent(string eventName, string eventAddress, string eventLink)
+    {
+        IEnumerable<Location?> locations = await Geocoding.Default.GetLocationsAsync(eventAddress);
+        var venueLocation = locations?.FirstOrDefault();
+        if (venueLocation == null)
+        {
+            return;
+        }
+        // We probably don't need the Date for the event, since events being added to this will be weekly, or monthly or whatever, and the 
+        // date and how frequently they occur will all be on the startgg page, which they will give the link for anyway
+        // we will leave it the db, and thus here as well, just in case the clients decide to change their minds later (may happen after final submission)
+        long id = (Events.Count == 0) ? 0 : Events.Last().EventId + 1;
+        var eventToAdd = new Event(id, eventName, eventAddress, DateTime.Now, DateTime.Now, venueLocation.Latitude,
+            venueLocation.Longitude, eventLink);
+        eventTable.Insert(eventToAdd);
+        Events.Add(eventToAdd);
+        AddPin(eventToAdd);
+    }
+
+
+    public void RemoveEvent(Event eventToRemove)
+    {
+        eventTable.Delete(eventToRemove.EventId);
+        RemovePin(eventToRemove);
+    }
+
+    private void AddPin(Event eventToAdd)
+    {
+        
+    }
+
+    private void RemovePin(Event eventToRemove)
+    {
+        
+    }
+    
 }
